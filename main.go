0ac7c5cf0bddc2f35890142da1522beb21a2e843
1069195546 <1069195546@qq.com>
2021-03-14 21:38:46 +0800

    更新了main

--- main.go
+++ main.go
@@ -11,19 +11,51 @@ import (
 	"github.com/xiaohuazjg/blog_service/internal/routers"
 	"github.com/xiaohuazjg/blog_service/pkg/logger"
 	"github.com/xiaohuazjg/blog_service/pkg/setting"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+
+var (
+	port string
+	runMode string
+	config string
+	isVersion bool
+
+)
 func init() {
-	err := setupSetting()
+
+	err:=setupFlag()
+	if err!=nil {
+		log.Fatalf("init.setupFlag err:v%",err)
+	}
+
+	err = setupSetting()
 	if err != nil {
-		log.Fatalf("init.setupSetting err%v")
+		log.Fatalf("init.setupSetting err:%v",err)
 	}
 	err = setupLogger()
 	if err != nil {
-		log.Fatal("init.setupLogger err:", err)
+		log.Fatal("init.setupLogger err:%v", err)
 	}
+	err=setupDBEngine(){
+       if err!=nil{
+		log.Fatal("init.setupDBEngine err:%v", err)
+	   }
+	}
+	err=setupValidator(){
+		if err!=nil{
+		 log.Fatal("init.setupValidator err:%v", err)
+		}
+	 }
+	 err=setupTracer(){
+		if err!=nil{
+		 log.Fatal("init.setupValidator err:%v", err)
+		}
+	 }
+
 }
+
 
 func setupSetting() error {
 	setting, err := setting.NewSetting()
@@ -66,9 +98,80 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+   go func(){
+	   if err:=s.ListenAndServe();err!=nil &&err!=http.ErrServerClosed {
+		   log.Fatalf("s.ListenAndServer() err:%v")
+	   }
+   }()
+   quit:=make(char os.Signal)
+   singnal.Notify(quit,syscall.SIGINT,syscall.SYSTEM)
+   <-quit
+   log.Println("Shuting downing server...")
+   
+   ctx,cancel :=context.WithTimeout(context,Background(),5*time.Second)
+	defer cancel()
+	if err:=s.Shutdown(ctx);err!=nil {
+		log.Fatalf("Server forced shutdwon:",err)
+
+	}
+     log.Println("Server Exiting")
+}
+
+func setupFlag() error {
+	flag.StringVar(&port,"port","","启动端口")
+	flag.StringVar(&runMode,"mode","","启动模式")
+	flag.StringVar(&config,"config","configs/","指定使用的配置文件路径")
+	flag.BoolVar(&isVersion,"version",false,"编译信息")
+    flag.Parse()
+    return nil 
 
 }
+
+func setupSetting() error {
+   s,err:=setting.NewSetting(stings.Split(config,",")...)
+   if err!=nil {
+	   return err
+
+   }
+   err=s.ReadSection("Server",&global.ServerSetting)
+   
+	   if err!=nil {
+		   return err
+	   }
+   
+   err=s.ReadSection("App",&global.AppSetting)
+	if err!=nil {
+		return err
+	}
+	err=s.ReadSection("Database",&global.DatabaseSetting)
+	if err!=nil {
+		return err
+	}
+	err=s.ReadSection("JWT",&global.JWTSetting)
+	if err!=nil {
+		return err
+	}
+	err=s.ReadSection("Email",&global.EmailSetting)
+	if err!=nil {
+		return err
+	}
+	global.AppSetting.DefaultContextTimeout*=time.Second
+	global.JWTSetting.Expire*=time.Second
+	global.ServerSetting.ReadTimeout*=time.Second
+	global.ServerSetting.WriteTimeout*=time.Second
+	
+	if port!="" {
+		global.ServerSetting.HttpPort=port
+
+	}
+	if runMode!="" {
+		global.ServerSetting.RunMode=runMode
+	}
+	return nil 
+
+}
+
+
 
 func setupDBEngine() error {
 	var err error
@@ -88,4 +191,15 @@ func setupLogger() error {
 		LocalTime: true,
 	}, "", log.LstdFlags).WithCaller(2)
 	return nil
+}
+
+
+func setupTracer() error {
+    jaegerTracer,_,err:=tracer.NewJadgerTrader("blog_service","127.0.0.1:6831")
+	if err!=nil {
+		return err
+	}
+	global.Tracer =jaegerTracer
+	return nil
+	
 }
